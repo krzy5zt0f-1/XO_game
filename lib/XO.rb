97e9415a1283c16e619fd433c2250640625113c0
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-11 17:18:56 +0100

    game menu done, now just need to work on README.md and imporve AI

--- lib/XO.rb
+++ lib/XO.rb
@@ -2,7 +2,6 @@
 require "terminal-table"
 # gem extension to colour X's and O's
 require 'colorize'
-
   # grid to be used
   $rows = [[" ", " ", " "], :separator, [" ", " ", " "],
   :separator, [" ", " ", " "]]
@@ -147,17 +146,61 @@ require 'colorize'
    end
  end
 
-# class that holds a front screen and a back screen
-class Animations
+# class that holds a front screen animation
+
+class Animation
 def front
-  i = 0
-  while i < 11 do
-    system "clear"
-    File.foreach("front#{i % 2}.txt") { |frame| puts frame.colorize(:red) }
-    sleep(0.5)
-    i += 1
-   end
+  for i in 1..13 do
+   system "clear"
+   puts i % 2 == 0 ? File.read(File.dirname(__FILE__) + '/front0.txt').colorize(:red) : File.read(File.dirname(__FILE__) + '/front0.txt').colorize(:green)
+   sleep(0.5)
+  end
+end
+def back
+
 end
 end
-h = Animations.new
-h.front
+
+# class to hold game menu
+
+class Menu
+def initialize
+  @g = Game.new
+  @a = Animation.new
+  menu
+end
+# choices of Menu
+def process(selection)
+  case selection
+  when "1"
+    puts "You'r goal is to cross three cells in vertical, horizontal"
+    puts "diagonal direction."
+    puts "To play, just input which cell you would like to cross and hit return."
+    puts "For example, `to_left` will mark top left corner of a grid, `mid_mid`"
+    puts "will mark the middle, `bottom_right` will mark bottom right corner etc."
+    puts " To exit a current game just type in `exit and hit return.`"
+  when "2"
+    @g.game_run
+  when "3"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+def menu_options
+  puts "--------------------------------------------------"
+  puts "1. Game instructions [press: 1]"
+  puts "2. Start game [press: 2]"
+  puts "3. Exit [press 3]"
+end
+def menu
+  @a.front
+  sleep (1)
+  loop do
+    menu_options
+    process(STDIN.gets.strip)
+  end
+end
+end
+
+t= Menu.new
